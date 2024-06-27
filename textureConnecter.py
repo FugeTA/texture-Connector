3ac6438c3554442ef49009932b9ccf59df49bec4
Fujita Tatsuki <132869827+FugeTA@users.noreply.github.com>
2024-06-27 19:31:56 +0900

    Update textureConnecter.py
    add language switch

--- textureConnecter.py
+++ textureConnecter.py
@@ -59,11 +59,11 @@ def nodecrate(s,i,nodeName):
     
     
 # パスの確認
-def checkPath(fullPath):
+def checkPath(fullPath,lan):
     if os.path.isfile(fullPath)==False:  # 設定されたパスに画像があるかチェック
-        missfile = pm.confirmDialog(t='Error',m=('Image file not found.\nPlease select material and check file path.\n'+'"'+fullPath+'"'),b=['Copy to clipboard','Close'])
-        if missfile == 'Copy to clipboard':
-            subprocess.run("clip", input=fullPath, text=True)
+        missfile = pm.confirmDialog(t='Error',m=errorlanguage(lan,fullPath)[1],b=[(errorlanguage(lan,'')[2]),(errorlanguage(lan,'')[3])])
+        if missfile == (errorlanguage(lan,'')[2]):
+            subprocess.run("clip", input=fullPath, text=True)  # WindowsのClipを使用してクリップボードにコピー
     return (os.path.isfile(fullPath))
             
 # テクスチャフォルダパスの調整
@@ -76,16 +76,16 @@ def projpath(texPath,name,s,f,imgformat):
     return(fullPath,imgPath)
 
 # 本体
-def texplace(nodeName,fileName,texPath,name,imgformat):
+def texplace(nodeName,fileName,texPath,name,imgformat,lan):
     s = pm.ls(sl=True)
     
     if s == []:
-        pm.confirmDialog(t='Error',m=('Plese select a Material'),b='Close')
+        pm.confirmDialog(t='Error',m=(errorlanguage(lan,'')[0]),b=(errorlanguage(lan,'')[3]))
         return
     
     for i,f in enumerate(fileName):
         path = projpath(texPath,name,s,f,imgformat)
-        if checkPath(path[0])==False:
+        if checkPath(path[0],lan)==False:
             break
         nodes = nodecrate(s,i,nodeName)
         Sorttex(f,nodes[0],nodes[1],nodes[2],path[1])
@@ -93,7 +93,7 @@ def texplace(nodeName,fileName,texPath,name,imgformat):
 # 変数の記憶
 def savecheck(ws):
     chlist = [0,0,0,0,0,0,0]
-    for i,c in enumerate(chlist):  # チェックリストの保存
+    for i in range(len(chlist)):  # チェックリストの保存
         chs = 'check'+str(i+1)
         chlist[i] = ws[chs].getValue()
     pm.optionVar['checklist'] = chlist  # データをuserPrefs.melに保存
@@ -112,6 +112,10 @@ def savefbx(ws):
 def saveformat(ws):
     imgformat = pm.optionMenu(ws['format'],q=True,sl=True)  # 画像形式の保存
     pm.optionVar['imgformat'] = imgformat  # データをuserPrefs.melに保存
+    
+def savelang(ws):
+    lan = pm.optionMenu(ws['lang'],q=True,sl=True)  # 画像形式の保存
+    pm.optionVar['texlanguage'] = lan  # データをuserPrefs.melに保存
 
 # 変数の呼び出し
 def loadvar():
@@ -131,7 +135,11 @@ def loadvar():
         imgformat = pm.optionVar['imgformat']
     else:
         imgformat = 1
-    return [chlist,texpath,fbxpath,imgformat]
+    if (pm.optionVar(ex='texlanguage')==True):
+        lan = pm.optionVar['texlanguage']
+    else:
+        lan = 1
+    return [chlist,texpath,fbxpath,imgformat,lan]
 
 # 入力リセット
 def resetvariable(ws):
@@ -139,13 +147,16 @@ def resetvariable(ws):
     pm.optionVar['texPath'] = 'sourceimages\\texture\\'
     pm.optionVar['fbxPath'] = ''
     pm.optionVar['imgformat'] = 1
+    pm.optionVar['lang'] = 1
     for i in range(6):
         chs = 'check'+str(i+1)
         ws[chs].setValue(0)
     ws['path'].setText('sourceimages\\texture\\')
     ws['fbx'].setText('')
     pm.optionMenu(ws['format'],e=True,sl=1)
-    pm.button(ws['button1'],e=True,en=False)
+    pm.optionMenu(ws['lang'],e=True,sl=1)
+    winlanguage(ws)
+    pm.button(ws['button3'],e=True,en=False)
 
 # ノード接続用の名前変更
 def namereplace(ws):
@@ -161,8 +172,9 @@ def namereplace(ws):
     texPath = ws['path'].getText()
     name = ws['fbx'].getText()
     formats = ['','png','exr','tif']
+    lan = pm.optionMenu(ws['lang'],q=True,sl=True)
     imgformat = formats[(pm.optionMenu(ws['format'],q=True,sl=True))]
-    texplace(nodeName,fileName,texPath,name,imgformat)
+    texplace(nodeName,fileName,texPath,name,imgformat,lan)
 
 # テクスチャフォルダの名前抽出
 def texPath(ws):
@@ -181,13 +193,38 @@ def fbxPath(ws):
 # チェックボックスが押されている時のみ実行ボタンを押せる
 def changeswitch(ws):
     savecheck(ws)
-    for i in range(6):
+    for i in range(7):
         checks = 'check'+str(i+1)
         if(ws[checks].getValue()):
-            pm.button(ws['button1'],e=True,en=True)
+            pm.button(ws['button3'],e=True,en=True)
             break
     else:
-        pm.button(ws['button1'],e=True,en=False)
+        pm.button(ws['button3'],e=True,en=False)
+
+# 言語設定
+def winlanguage(ws):
+    en = ['BaseColor','Metalness','Roughness','Normal','Height','Emissive','Opacity','Texture Folder','FBX file','Connect','Close','Reset','ImageFormat','Language']
+    jp = ['BaseColor','Metalness','Roughness','Normal','Height','Emissive','Opacity','テクスチャフォルダ','FBXファイル','接続','閉じる','リセット','画像形式','言語']
+    if (pm.optionMenu(ws['lang'],q=True,sl=True))==1:
+        ln = en
+    else:
+        ln = jp
+    for i in range(7):
+        chs = 'check'+str(i+1)
+        pm.checkBox(ws[chs],e=True,l=ln[i])
+    for i in range(5):
+        btn = 'button'+str(i+1)
+        pm.button(ws[btn],e=True,l=ln[i+7])
+    pm.optionMenu(ws['format'],e=True,l=ln[12])
+    pm.optionMenu(ws['lang'],e=True,l=ln[13])
+    savelang(ws)
+    
+def errorlanguage(lan,fullPath):
+    en = ['Plese select a Material','Image file not found.\nPlease select material and check file path.\n'+'"'+fullPath+'"','Copy to clipboard','Close']
+    jp = ['マテリアルを選択してください。','画像が見つかりません。\n正しいマテリアルを選択しているか、またはパスを確認してください。\n'+'"'+fullPath+'"','クリップボードにコピー','閉じる']
+    if lan==1:
+        return en
+    return jp
 
 # ウィンドウ作成
 def openWindow():
@@ -206,31 +243,39 @@ def openWindow():
     with pm.window(winname):  # ウィンドウの作成
         with pm.autoLayout():
             ws = {}
-            pm.button(l='Reset', c=pm.Callback(resetvariable,ws))  # リセット
             with pm.horizontalLayout():
-                ws['check1'] = pm.checkBox(l='BaseColor',v=ch1,cc=pm.Callback(changeswitch,ws))  # 接続したい画像の指定
-                ws['check2'] = pm.checkBox(l='Metalness',v=ch2,cc=pm.Callback(changeswitch,ws))
-                ws['check3'] = pm.checkBox(l='Roughness',v=ch3,cc=pm.Callback(changeswitch,ws))
+                ws['lang'] = pm.optionMenu(l='言語',cc=pm.Callback(winlanguage,ws))  # 画像形式の指定
+                pm.menuItem(l="English")
+                pm.menuItem(l="日本語")
+                ws['button5'] = pm.button(c=pm.Callback(resetvariable,ws))  # リセット
             with pm.horizontalLayout():
-                ws['check4'] = pm.checkBox(l='Normal',v=ch4,cc=pm.Callback(changeswitch,ws))
-                ws['check5'] = pm.checkBox(l='Height',v=ch5,cc=pm.Callback(changeswitch,ws))
-                ws['check6'] = pm.checkBox(l='Emissive',v=ch6,cc=pm.Callback(changeswitch,ws))
-                ws['check7'] = pm.checkBox(l='Opacity',v=ch7,cc=pm.Callback(changeswitch,ws))
+                ws['check1'] = pm.checkBox(v=ch1,cc=pm.Callback(changeswitch,ws))  # 接続したい画像の指定
+                ws['check2'] = pm.checkBox(v=ch2,cc=pm.Callback(changeswitch,ws))
+                ws['check3'] = pm.checkBox(v=ch3,cc=pm.Callback(changeswitch,ws))
+            with pm.horizontalLayout():
+                ws['check4'] = pm.checkBox(v=ch4,cc=pm.Callback(changeswitch,ws))
+                ws['check5'] = pm.checkBox(v=ch5,cc=pm.Callback(changeswitch,ws))
+                ws['check6'] = pm.checkBox(v=ch6,cc=pm.Callback(changeswitch,ws))
+                ws['check7'] = pm.checkBox(v=ch7,cc=pm.Callback(changeswitch,ws))
                 
             with pm.horizontalLayout():
-                ws['path'] = pm.textField(ann='TextureFolder',text=texpath,cc=pm.Callback(savetex,ws))  # テクスチャフォルダの指定（画像を格納しているフォルダ）
-                pm.button(l='Texture Folder',c=pm.Callback(texPath,ws))
+                ws['path'] = pm.textField(text=texpath,cc=pm.Callback(savetex,ws))  # テクスチャフォルダの指定（画像を格納しているフォルダ）
+                ws['button1'] = pm.button(c=pm.Callback(texPath,ws))
             with pm.horizontalLayout():
-                ws['fbx'] = pm.textField(ann='FBXFile',text=fbxpath,cc=pm.Callback(savefbx,ws))  # FBXファイルの指定
-                pm.button(l='FBX file', c=pm.Callback(fbxPath,ws))
+                ws['fbx'] = pm.textField(text=fbxpath,cc=pm.Callback(savefbx,ws))  # FBXファイルの指定
+                ws['button2'] = pm.button(c=pm.Callback(fbxPath,ws))
                 
-            ws['format'] = pm.optionMenu(l='ImageFormat',cc=pm.Callback(saveformat,ws))  # 画像形式の指定
+            ws['format'] = pm.optionMenu(cc=pm.Callback(saveformat,ws))  # 画像形式の指定
             ws['menu1'] = pm.menuItem(l="PNG")
             ws['menu2'] = pm.menuItem(l="EXR")
             ws['menu3'] = pm.menuItem(l="TIFF")
             pm.optionMenu(ws['format'],e=True,sl=imgformat)  # 画像形式の呼び出し
             
             with pm.horizontalLayout():
-                ws['button1'] = pm.button(l='Connect', c=pm.Callback(namereplace, ws),en=False)  # 本体の実行
-                pm.button(l='Close', c=pm.Callback(pm.deleteUI,winname))  # クローズボタンで閉じたときの処理
+                ws['button3'] = pm.button(c=pm.Callback(namereplace, ws),en=False)  # 本体の実行
+                ws['button4'] =pm.button(c=pm.Callback(pm.deleteUI,winname))  # クローズボタンで閉じたときの処理
+            pm.optionMenu(ws['lang'],e=True,sl=load[4])
+            winlanguage(ws)
             changeswitch(ws)  # 実行可能かの確認
+            
+openWindow()
