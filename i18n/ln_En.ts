256fc3db98af293256c7a1b736714acc4ad73a41
Fujita Tatsuki <132869827+FugeTA@users.noreply.github.com>
2024-10-17 14:55:37 +0900

    Add files via upload

---
+++ i18n/ln_En.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+    <name>ErrorWindow</name>
+    <message>
+        <location filename="../textureConnecter_Qt.py" line="14"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="17"/>
+        <source>Plese select a Material</source>
+        <translation>Plese select a Material</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="17"/>
+        <source> file not found.
+Please check file path.
+</source>
+        <translation> file not found.\nPlease check file path.\n</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="19"/>
+        <source>The image file and material name do not match.
+Please make sure you have selected the correct material.</source>
+        <translation>The image file and material name do not match.\nPlease make sure you have selected the correct material.</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="25"/>
+        <source>Copy to clipboard</source>
+        <translation>Copy to clipboard</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="58"/>
+        <source>reset</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="133"/>
+        <source>Connect</source>
+        <translation>Connect</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="136"/>
+        <source>Close</source>
+        <translation>Close</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/ln_jp.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="en">
+<context>
+    <name>ErrorWindow</name>
+    <message>
+        <location filename="../textureConnecter_Qt.py" line="14"/>
+        <source>Error</source>
+        <translation>エラー</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="17"/>
+        <source>Plese select a Material</source>
+        <translation>マテリアルを選択してください</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="17"/>
+        <source> file not found.
+Please check file path.
+</source>
+        <translation> ファイルが見つかりません。\n以下のフォルダに画像があるかを確認してください。\n</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="19"/>
+        <source>The image file and material name do not match.
+Please make sure you have selected the correct material.</source>
+        <translation>画像ファイルとマテリアル名が一致しません。\n正しいマテリアルを選択しているか確認してください。</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="25"/>
+        <source>Copy to clipboard</source>
+        <translation>クリップボードにコピー</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="58"/>
+        <source>reset</source>
+        <translation>リセット</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="133"/>
+        <source>Connect</source>
+        <translation>接続</translation>
+    </message>
+    <message>
+        <location filename="textureConnecter_Qt.py" line="136"/>
+        <source>Close</source>
+        <translation>閉じる</translation>
+    </message>
+</context>
+</TS>
